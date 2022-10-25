7884ee8f47d01c3c16fa120051e31e4c75d40c31
Sergei Slobodchikov <cerhclobodhikow@mail.ru>
2022-10-25 19:55:15 +0300

    krestiki noliki

--- Program.cs
+++ Program.cs
@@ -1,57 +1,4 @@
-﻿
-// Задача 19
-
-// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
-
-// 14212 -> нет
-
-// 12821 -> да
-
-// 23432 -> да
-
-// int InputInt(string value)
-// {
-//     int number = 0;
-//     while (!int.TryParse(value, out number))
-//     {
-//         Console.Write("Не подходит к требованиЮ введите число: ");
-//         value = Console.ReadLine();
-//     }
-//     return number;
-// }
-// void Palindrom(int first, int dlinaNum, int num)
-// {
-//     int i = 1;
-//     while (dlinaNum > i)
-//     {
-//         first = (int)((num / Math.Pow(10, (dlinaNum - i))) % 10);
-//         int last = num % 10;
-//         Console.Write($"{first}");
-//         Console.WriteLine($"{last}");
-//         dlinaNum = dlinaNum - 1;
-//         i++;
-//         num = num / 10;
-//         if (first != last)
-//         {
-//             Console.WriteLine("не палиндром");
-//             return;
-//         }
-//     }
-//     Console.WriteLine("Палиндром");
-// }
-// Console.Write("Введите пятизначное число: ");
-// int num = InputInt(Console.ReadLine());
-// int first = 0;
-// string x = Convert.ToString(num);
-// int dlinaNum = x.Length;
-// int i = 1;
-// if (num > 9)
-// {
-//     Palindrom(first, dlinaNum, num);
-// }
-// else Console.WriteLine($"не подходит по требованиям");
-
-/////////////////////////////////////
+﻿/////////////////////////////////////
 ///////////////////////////////////////
 char[,] field = new char[4, 4] {                                             //// Массив сделал 4 на 4 чтобы использовать строки и столбцы 1-3
         { ' ', ' ', ' ' , ' ' },
@@ -106,6 +53,7 @@ for (int i = 1; i < 6; i++)                                                 ////
     field[str, column] = nolik;
     Console.Clear();
     pobeda(field, ref schet);
+    x(ref schet, ref field);
     if (schet == 10)
     {
         Console.WriteLine("конец игры");
@@ -119,6 +67,31 @@ void pobeda(char[,] field, ref int schet)                                   ////
 
     schet++;
     if (schet == 9)
+        win = (field[1, 1] != ' ' && field[1, 1] == field[1, 2] && field[1, 2] == field[1, 3] ||
+                   field[2, 1] != ' ' && field[2, 1] == field[2, 2] && field[2, 2] == field[2, 3] ||
+                   field[3, 1] != ' ' && field[3, 1] == field[3, 2] && field[3, 2] == field[3, 3] ||
+                   field[1, 1] != ' ' && field[1, 1] == field[2, 1] && field[2, 1] == field[3, 1] ||
+                   field[1, 2] != ' ' && field[1, 2] == field[2, 2] && field[2, 2] == field[3, 2] ||
+                   field[1, 3] != ' ' && field[1, 3] == field[2, 3] && field[2, 3] == field[3, 3] ||
+                   field[1, 1] != ' ' && field[1, 1] == field[2, 2] && field[2, 2] == field[3, 3] ||
+                   field[1, 3] != ' ' && field[1, 3] == field[2, 2] && field[2, 2] == field[3, 1]);
+
+    if (win == true)
+    {
+        Console.WriteLine(field[str, column] == 'x' ? "Победили крестики" : "Победили Нолики");
+        schet = 10;
+        return;
+    }
+    else if (schet == 10)
+    {
+        Console.WriteLine("Ничья");
+        return;
+
+    }
+}
+void x(ref int schet,ref char[,] field)
+{
+    if (schet == 9)
     {
 
         for (int i = 1; i < 4; i++)
@@ -128,40 +101,24 @@ void pobeda(char[,] field, ref int schet)                                   ////
                 if (field[i, j] == ' ')
                 {
                     field[i, j] = 'x';
-                    schet = 10;
                 }
             }
 
         }
-
-    }
-    win = (field[1, 1] != ' ' && field[1, 1] == field[1, 2] && field[1, 2] == field[1, 3] ||
-               field[2, 1] != ' ' && field[2, 1] == field[2, 2] && field[2, 2] == field[2, 3] ||
-               field[3, 1] != ' ' && field[3, 1] == field[3, 2] && field[3, 2] == field[3, 3] ||
-               field[1, 1] != ' ' && field[1, 1] == field[2, 1] && field[2, 1] == field[3, 1] ||
-               field[1, 2] != ' ' && field[1, 2] == field[2, 2] && field[2, 2] == field[3, 2] ||
-               field[1, 3] != ' ' && field[1, 3] == field[2, 3] && field[2, 3] == field[3, 3] ||
-               field[1, 1] != ' ' && field[1, 1] == field[2, 2] && field[2, 2] == field[3, 3] ||
-               field[1, 3] != ' ' && field[1, 3] == field[2, 2] && field[2, 2] == field[3, 1]);
-
-    if (win == true)
-    {
-        if (schet == 10)
+        win = (field[1, 1] != ' ' && field[1, 1] == field[1, 2] && field[1, 2] == field[1, 3] ||
+                   field[2, 1] != ' ' && field[2, 1] == field[2, 2] && field[2, 2] == field[2, 3] ||
+                   field[3, 1] != ' ' && field[3, 1] == field[3, 2] && field[3, 2] == field[3, 3] ||
+                   field[1, 1] != ' ' && field[1, 1] == field[2, 1] && field[2, 1] == field[3, 1] ||
+                   field[1, 2] != ' ' && field[1, 2] == field[2, 2] && field[2, 2] == field[3, 2] ||
+                   field[1, 3] != ' ' && field[1, 3] == field[2, 3] && field[2, 3] == field[3, 3] ||
+                   field[1, 1] != ' ' && field[1, 1] == field[2, 2] && field[2, 2] == field[3, 3] ||
+                   field[1, 3] != ' ' && field[1, 3] == field[2, 2] && field[2, 2] == field[3, 1]);
+        if (win == true)
         {
             Console.WriteLine("Победили крестики");
+            schet = 10;
             return;
         }
-        else
-        {
-            Console.WriteLine(field[str, column] == 'x' ? "Победили крестики" : "Победили Нолики");
-            return;
-        }
-    }
-    else if (schet == 10)
-    {
-        Console.WriteLine("Ничья");
-        return;
-
     }
 }
 
